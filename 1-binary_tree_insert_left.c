2dec23f91291b60f43ff3b2477245679ffe24ded
edin93 <il_matador@hotmail.fr>
2020-03-10 12:50:31 +0100

    task 1 done

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,4 +8,19 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *btn = malloc(sizeof(binary_tree_t));
+
+	if (btn == NULL || parent == NULL)
+		return (NULL);
+	btn->n = value;
+	btn->parent = parent;
+	btn->right = NULL;
+	btn->left = NULL;
+	if (parent->left != NULL)
+	{
+		parent->left->parent = btn;
+		btn->left = parent->left;
+	}
+	parent->left = btn;
+	return (btn);
 }
