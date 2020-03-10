09da2d83fb1b6056257fcb6f3e40c5e2e4050da4
taiebchaabini <taiebchaabini@gmail.com>
2020-03-10 21:10:51 +0000

    First commit new file / Betty coding style fix

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_preorder - function that goes through a binary tree using
- * pre-order traversal 
+ * pre-order traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node. The value in the node
  * must be passed as a parameter to this function.
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,19 +1,20 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - function that goes through a binary tree using
- * in-order traversal
+ * binary_tree_postorder - function that goes through a binary tree using
+ * post-order traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node. The value in the node
  * must be passed as a parameter to this function.
 **/
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+
 	if (tree)
 	{
-		binary_tree_inorder(tree->left, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
 		func(tree->n);
-		binary_tree_inorder(tree->right, func);
 	}
 }
