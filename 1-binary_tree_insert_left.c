47030d7293a0bd02eaaee982ad0200991f1c30c9
edin93 <il_matador@hotmail.fr>
2020-03-11 17:39:50 +0100

    task 1 dump change

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,8 +8,9 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *btn = malloc(sizeof(binary_tree_t));
+	binary_tree_t *btn;
 
+	btn = malloc(sizeof(binary_tree_t));
 	if (btn == NULL || parent == NULL)
 		return (NULL);
 	btn->n = value;
