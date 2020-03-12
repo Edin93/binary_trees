e77c32c802b9ef4eb0352fd98aca262fb46b918f
edin93 <il_matador@hotmail.fr>
2020-03-12 15:19:41 +0100

    task 100 done

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,4 +1,22 @@
 #include "binary_trees.h"
+/**
+ * depth - measures the depth of a node in a binary tree.
+ * @tree: pointer to the node to measure the depth.
+ * Return: the depth calculated. if tree is NULL return 0.
+ */
+size_t depth(const binary_tree_t *tree)
+{
+	size_t d = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree->parent)
+	{
+		d += 1;
+		tree = tree->parent;
+	}
+	return (d);
+}
 
 /**
  * binary_trees_ancestor - function that finds the lowest common ancestor
@@ -8,27 +26,41 @@
  * Return: pointer to the lowest common ancestor otherwise NULL
 **/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+				     const binary_tree_t *second)
 {
+	binary_tree_t *ancestor = NULL;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
-	while (first->parent != NULL)
+	while (first != second)
 	{
-		while (second->parent != NULL)
+		if (first->parent == second->parent)
 		{
-			if (first->parent->n == second->parent->n ||
-					first->n == second->parent->n)
-			{
-				if (second->parent->n < first->n)
-					return (second->parent);
-				else
-					return (first->left->parent);
-			}
-			second = second->parent;
+			return (first->parent);
 		}
-
-		first = first->parent;
+		else if (depth(first) == depth(second) && first != second)
+		{
+			if (first->parent != NULL && second->parent != NULL)
+			{
+				first = first->parent;
+				second = second->parent;
+				ancestor = first->parent;
+			}
+		}
+		else if (depth(first) > depth(second))
+		{
+			if (first->parent == second)
+				return (first->parent);
+			first = first->parent;
+			ancestor = first->parent;
+		}
+		else if (depth(first) < depth(second))
+		{
+			if (second->parent == first)
+				return (second->parent);
+			second = second->parent;
+			ancestor = second->parent;
+		}
 	}
-	return (NULL);
+	return (ancestor);
 }
