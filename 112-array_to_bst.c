ae7410010818977d15c55972698407858ffc0989
taiebchaabini <taiebchaabini@gmail.com>
2020-03-13 20:09:00 +0000

    Adding description to the main fn

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 /**
- * array_to_bst - unction that builds a Binary Search Tree from an array
+ * array_to_bst - function that builds a Binary Search Tree from an array
  * @array: pointer to the first element of the array to be converted
  * @size: the number of element in the array
- *
+ * Return: root of the new builded Binary Search Tree otherwise returns NULL
+ * if failure
 **/
 bst_t *array_to_bst(int *array, size_t size)
 {
