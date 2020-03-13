fe8455fb5d908858c284c19e9cfeab5c872377e7
taiebchaabini <taiebchaabini@gmail.com>
2020-03-13 14:24:33 +0000

    Adding typedef for Binary Search Tree & AVL Tree & Max Binary Heap

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,9 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -46,4 +49,5 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 #endif
